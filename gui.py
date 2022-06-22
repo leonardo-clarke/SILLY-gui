dfce2f1153a0d82071c20ae3eb8a9aa68d7e44d9
zohaggard <zoehaggard@gmail.com>
2022-06-22 16:47:03 -0700

    toggle button added

--- gui.py
+++ gui.py
@@ -14,13 +14,13 @@ from matplotlib.figure import Figure
 
 matplotlib.use('QtAgg')
 
-class MplCanvas(FigureCanvasQTAgg):
+class mplCanvas(FigureCanvasQTAgg):
 
     def __init__(self, parent=None, width=5, height=4, dpi=100):
 
         fig = Figure(figsize=(width, height), dpi=dpi)
         self.axes = fig.add_subplot(111)
-        super(MplCanvas, self).__init__(fig)
+        super(mplCanvas, self).__init__(fig)
 
 class ApplicationWindow(QMainWindow):
     
@@ -28,20 +28,41 @@ class ApplicationWindow(QMainWindow):
 
         super().__init__()
 
+        layout = QVBoxLayout()
+        widget = QWidget()
+        widget.setLayout(layout)
+        self.setCentralWidget(widget)
+
         self.setWindowTitle('silly-GUI')
         self.setGeometry(200, 200, 350, 350)
+        
         self.filename = ''
         self.df = pd.DataFrame()
+        self.wave_range = []
+        
+        self.canv = mplCanvas()
+        layout.addWidget(self.canv)
+
         self.add_toolbar()
 
     def add_toolbar(self):
 
         toolbar = QToolBar()
         self.addToolBar(toolbar)
-        button_action = QAction('upload file', self)
-        #button_action.setStatusTip("This is your button")
-        button_action.triggered.connect(self.getfile)
-        toolbar.addAction(button_action)
+
+        upload_file = QAction('upload file', self)
+        upload_file.triggered.connect(self.getfile)
+        toolbar.addAction(upload_file)
+
+        toggle_drag = QAction('toggle', self)
+        toggle_drag.triggered.connect(self.selectrange)
+        toolbar.addAction(toggle_drag)
+
+
+    def selectrange(self):
+        
+        return
+
 
     def getfile(self):
 
@@ -53,29 +74,15 @@ class ApplicationWindow(QMainWindow):
 
         self.df = pd.read_csv(self.filename, header=0)
         print(self.df)
-		#self.Update(self.themes[0]) 
+        self.update() 
     
     def update(self):
         
-        return
+        self.canv.axes.cla()
+        self.df.plot(x = self.df.columns[1], y = self.df.columns[2], ax = self.canv.axes)
+        self.canv.draw()
+        #sc.axes.plot([0,1,2,3,4], [10,1,20,3,40])
 
-
-
-        
-
-    #     menuBar = self.menuBar()
-    # def read_data(self):
-    #     return
-    
-    # def menu(self):
-    #     return
-# lmin = 500
-# lmax = 510
-# a = 1
-# mean = 505
-# sigma = 1
-
-# toy = toy_data.gaussian_noise(lmin, lmax, a, mean, sigma)
 if (__name__ == '__main__'):
     application = QApplication([])
     mainWindow = ApplicationWindow()
