65573d7f39a998d17daddef333b0b741f047f8bc
leonardo-clarke <leonardogclarke@gmail.com>
2022-06-24 15:25:34 -0700

    setup changes

--- setup.py
+++ setup.py
@@ -1,10 +1,10 @@
 from setuptools import setup, find_packages
+import numpy
 
 setup(
-
-    name="silly",
-
-    version="1.0.0",
-
+    name='silly',
+    version='1.0.0',
+    include_package_data = True,
+    include_dirs=[numpy.get_include()],
     packages=find_packages()
 )
