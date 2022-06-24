5a195628fd4fa2af67c6504384c4f600fc146c45
leonardo-clarke <leonardogclarke@gmail.com>
2022-06-24 16:56:31 -0700

    pypi changes

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ import numpy
 
 setup(
     name='SILLY-gui',
-    version='1.0.2',
+    version='1.0.3',
     include_package_data = True,
     include_dirs=[numpy.get_include()],
     packages=find_packages()
